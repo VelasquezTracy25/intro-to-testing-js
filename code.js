5f0797c1d87594f207e84b00e5dee31d95757619
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-06-25 20:36:26 -0500

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -10,17 +10,8 @@ function helloWorld() {
 //         return false;
 //     }
 // }
-//
-// // sayHello() function that takes in a name as an argument and returns a string that says hello to that name.
-// function sayHello(name) {
-// return "Hello, " + name + "!";
-// }
 
 function sayHello(name){
-if (name=== "Alex") {
-    return "Hello, Alex!"
-}else if (name=== "Pat") {
-    return "Hello, Pat!"
-} else {return "Hello, Jane!";
-    }
+return "Hello, " + name + "!";
 }
+
