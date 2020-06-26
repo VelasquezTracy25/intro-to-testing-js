7486cfe6c4e3e6189278996ebbc7388b6d46bce2
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-06-25 20:15:20 -0500

    add the firsttest and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,18 +1,20 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
 }
 
-function isEven(param) {
-    if (param === 10) {
-        return true
-    } else {
-        return false;
-    }
-}
+// function isEven(param) {
+//     if (param === 10) {
+//         return true
+//     } else {
+//         return false;
+//     }
+// }
+//
+// // sayHello() function that takes in a name as an argument and returns a string that says hello to that name.
+// function sayHello(name) {
+// return "Hello, " + name + "!";
+// }
 
-// sayHello() function that takes in a name as an argument and returns a string that says hello to that name.
-
-function sayHello(name) {
-return "Hello, " + name;
+function sayHello(){
 }
--- tests.js
+++ tests.js
@@ -13,28 +13,46 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+//
+// // Tests for an even number
+// describe ("isEven", function() {
+//     it('should be a defined function', function(){
+//         expect(typeof isEven).toBe("function");
+//     });
+//     it('should return a boolean value', function() {
+//         expect(typeof isEven()).toBe("boolean");
+//     });
+//       it('should return true when passed a value of 10', function(){
+//       expect(isEven(10)).toBe(true);
+//     });
+// });
+//
+//
+// // Unit tests for the sayHello function
+// describe('sayHello', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof helloWorld).toBe('function');
+//     });
+//     it("should return a string when called", function() {
+//         expect (typeof sayHello()).toBe('string')
+//     });
+//     it('should return the string "Hello, (name)" when executed', function() {
+//         expect(sayHello(name)).toBe("Hello, " + name + "!");
+//     });
+//     it('should return the string "Hello, Jane!" when passed the input of ("Jane")', function() {
+//         expect(sayHello('Jane')).toBe("Hello, Jane!");
+//     });
+//     it('should return the string "Hello, Alex!" when passed the input of ("Alex")', function() {
+//         expect(sayHello('Alex')).toBe("Hello, Alex!");
+//     });
+// });
+//
 
-// Tests for an even number
-describe ("isEven", function() {
-    it('should be a defined function', function(){
-        expect(typeof isEven).toBe("function");
-    });
-    it('should return a boolean value', function() {
-        expect(typeof isEven()).toBe("boolean");
-    });
-      it('should return true when passed a value of 10', function(){
-      expect(isEven(10)).toBe(true);
-    });
-});
-
-
-// Unit tests for the sayHello function
 describe('sayHello', function() {
-    it('should return a string when called "Hello, (name)!"', function() {
-        expect(sayHello(name)).toBe("Hello, " + name);
-    });
+    it ('should be a defined function', function() {
+        expect (typeof helloWorld).toBe('function');
+        });
 });
-
 
 
 
